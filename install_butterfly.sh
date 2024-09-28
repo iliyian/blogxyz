f6639134ea04fa6577653c2cdb7aa311fe2e0a25
iliyian <i@iliyian.com>
2024-09-28 19:20:13 +0800

    use iliyian/hexo-theme-butterfly

--- install_butterfly.sh
+++ install_butterfly.sh
@@ -1,2 +1,2 @@
 rm -rf themes/butterfly
-git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+git clone -b master https://github.com/iliyian/hexo-theme-butterfly.git themes/butterfly
